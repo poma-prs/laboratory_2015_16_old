54d0ca14540fa37737dd2c81aee868da619b0a23
poma.prs <poma.prs@gmail.com>
2015-12-15 22:45:13 +0400

    add buzzer manager

---
+++ Информатор (Димов, Поскребышев)/Лабораторная работа 2/src/Buzzer/Buzzer/Models/BuzzerManager.cs
@@ -0,0 +1,28 @@
+﻿using Buzzer.Interface;
+
+namespace Buzzer.Models
+{
+    public class BuzzerManager : IBuzzerManager
+    {
+        private readonly NotificationManager _notificationManager;
+        private readonly AudioManager _audioManager;
+
+        public BuzzerManager(
+            NotificationManager notificationManager, 
+            AudioManager audioManager)
+        {
+            _notificationManager = notificationManager;
+            _audioManager = audioManager;
+        }
+
+        public void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
